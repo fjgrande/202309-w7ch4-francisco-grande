8d1686de3dc27cd5a6812b12ab08a5e6f68d8f27
Francisco Grande <fjgrandv@gmail.com>
2023-10-27 01:10:26 +0200

    Add AppStyled for App component

---
+++ src/components/App/AppStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  background-color: #bb8d2a;
+  font-size: 2rem;
+  width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+export default AppStyled;
